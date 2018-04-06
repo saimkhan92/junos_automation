c86b050f4ff2a7302aca94331e9b0899aaf692e1
Saim <saim.khan@colorado.edu>
2018-04-06 08:14:20 -0700

    handled exceptions, optimized code

--- apply_threaded_config.py
+++ apply_threaded_config.py
@@ -8,6 +8,9 @@ import csv
 import os
 from subprocess import call
 import time
+from jnpr.junos.exception import ConnectError
+from jnpr.junos.exception import ConfigLoadError
+from jnpr.junos.exception import CommitError
 
 def config_devices(row):
         set_commands_filename=row["skyent_hostname"]+".txt"
@@ -23,24 +26,29 @@ def config_devices(row):
 			response = os.system("ping -c 1 "+row["mgmt_ip"])	
 	max_tries=25
 	while max_tries>0:
-		try:
-			
+		try:	
 			with Device(host=row["mgmt_ip"], user="root", password="juniper1") as dev:
+				print("Established NETCONF connection to the device "+row["skyent_hostname"])
+				time.sleep(30)
 				fh=open(set_commands_filename,"r")
 				set_commands_list=fh.read().splitlines()
 				cu=Config(dev)
 				for command in set_commands_list:
 					if command:
-						cu.load(command)
-
-				if cu.commit():
-					print("Configuration successfully commited")
+						try:
+							cu.load(command)
+						except ConfigLoadError as err:
+							print("Encountered a config load exception {0}".format(err))
+							return False
+				try:
+					cu.commit()
+					print("SUCCESSFUL COMMIT COMPLETED. DEVICE "+row["skyent_hostname"]+" SHOULD BE ONLINE IN SKY ENTERPRISE!!")
 					return True
-				else:
-					print("Error in committing config")	
+				except CommitError as err:
+					print("Emcountered a commit exception {}".format(err))
 					return False
-		except:
-			print("Waiting for netconf connection to be established. Tries left: "+str(max_tries))
+		except ConnectError as err:
+			print("Waiting for netconf connection to be established. Tries left: "+str(max_tries)+"\n"+"Exception encountered: {}".format(err))
 			max_tries=max_tries-1
 			time.sleep(10)
 	print("Unable to establish netconf connection")
