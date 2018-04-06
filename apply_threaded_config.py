c8da0079e59d64bb08d587dae1cfca599afaccee
Saim <saim.khan@colorado.edu>
2018-04-05 17:55:08 -0700

    Applies config to devices uing threads

---
+++ apply_threaded_config.py
@@ -0,0 +1,56 @@
+# Script reads device(s) information from csv file, reads config (set format) from a file stored in current directory
+# Applies config to devices uing threads
+
+from jnpr.junos import Device
+from jnpr.junos.utils.config import Config
+from threading import Thread
+import csv
+import os
+
+config_devices(row):
+        set_commands_filename=row["skyent_hostname"]+".txt"
+        print("Executing thread for "+row["skyent_hostname"]+" "+row["mgmt_ip"])
+        response = os.system("ping -c 1 " + row["mgmt_ip"])
+        while True:
+                if response==0:
+                        print("Device is up!!")
+                        break
+                else:
+                        print(row["skyent_hostname"]+" device is down")
+                        time.sleep(20)
+			response = os.system("ping -c 1 " + row["mgmt_ip"])
+	
+	max_tries=25
+	while max_tries>0:
+		try:
+			
+			with Device(host=row["mgmt_ip"], user="root", password="juniper1") as dev:
+				fh=open(set_commands_filename,"r")
+				set_commands_list=fh.read().splitlines()
+				cu=Config(dev)
+				for command in set_commands_list:
+					if command:
+						cu.load(command)
+
+				if cu.commit():
+					print("Configuration successfully commited")
+					return True
+				else:
+					print("Error in committing config")	
+					return False
+		except:
+			print("Waiting for netconf connection to be established. Tries left: "+str(maxtries))
+			maxtries=maxtries-1
+			time.sleep(10)
+	print("Unable to establish netconf connection")
+	return False
+			
+def main():
+        csv_filename="host_to_mac_mapping.csv"
+        device_data = csv.DictReader(open(csv_filename))
+        for row in device_data:
+                t = Thread(target=config_devices, args=(row,))
+                t.start()
+
+if __name__=="__main__":
+        main()
